99d6119721699ce2eca77473382fbdb143c2b381
Hanjara Cahya Adhyatma <mangatkk@gmail.com>
2018-05-19 18:40:53 +0700

    configure auto

--- kalibrasi/M_WIFI/M_WIFI.h
+++ kalibrasi/M_WIFI/M_WIFI.h
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-SoftwareSerial mySerial(10, 11); // RX, TX
+SoftwareSerial sr(10, 11); // RX, TX
 
 void setup_wifi(){
   Serial.begin(115200);
@@ -10,15 +10,28 @@ void setup_wifi(){
   }
   
   // set the data rate for the SoftwareSerial port
-  mySerial.begin(115200);
+  sr.begin(115200);
 }
 
 void test_wifi() {
-  if (mySerial.available()) {
-    Serial.write(mySerial.read());
+  if (sr.available()) {
+    Serial.write(sr.read());
   }
-  if (Serial.available()) {
-    mySerial.write(Serial.read());
+//  if (Serial.available()) {
+//    sr.write(Serial.read());
+//  }
+}
+
+void AT(int i) {
+  switch(i){
+    case 0:
+      sr.write("AT\r\n");
+      break;
+    case 1:
+      sr.write("AT+GMR\r\n");
+      break;
+    default:
+      sr.write("AT\r\n");
   }
 }
 
